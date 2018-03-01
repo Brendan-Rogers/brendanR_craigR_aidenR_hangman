a4913922a2ea54e5f65fee60f7c3458f93debf26
cdr <c_roux2@fanshaweonline.ca>
2018-03-01 10:12:10 -0500

    Most likely wrong

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(()=> { console.log("hangman script initiated.")	
+(()=> { console.log("hangman script initiated.")
 
 // VARIABLES
 const words = ["blue", "orange", "yellow", "magenta", "violet"];
@@ -7,8 +7,24 @@ let initButton = document.querySelector('button');
 
 let currentWord = null,
 	wordHint = document.querySelector('.hint-string'),
-	guessBox = document.querySelector('.user-guess');
+	guessBox = document.querySelector('.user-guess'),
+	wrongGuesses = 0,
+	reserScreen = document.querySelector('rest-screen'),
+	resetButton = resetScreen.querySelector('button');
 
+	function restGame(){
+		debugger;
+		//reset the game and pick a new word
+		let gamePieces = Array.from(document.querySelectorAll('.show-piece'));
+		gamePieces.forEach(piece => piece.classList)
+	}
+
+
+	function showResetScreen(){
+		//user has lost, reset stuff and start over
+		debugger;
+		console.log('you lose, loser!');
+	}
 // FUNCTIONS
 function init() {
 	// look at MDN -> the Math object
@@ -16,9 +32,9 @@ function init() {
 	words[Math.floor(Math.random()*words.length)];
 
 	// map takes existing information in an array and transforms it
-	// fills hint with udnerscores
+	// fills hint with underscores
 	wordHint.textContext = currentWord.split("").map(letter => letter = "__").join(" ");
-	
+
 
 	// featured below: template string
 	console.log(`Guess this word: ${currentWord}. It's at ${words.indexOf(currentWord)}. Good Luck!`);
@@ -31,7 +47,23 @@ function takeGuess() {
 	// OR is ||
 	if (this.value == "" || this.value.length <1 ) {
 		return;
-	}	
+	}
+
+	//set up the win and lose paths (if/else)
+	if(!currentWord.includes(this.value)){
+		console.log('invalid letter!');
+		document.querySelector(`.wrong${wrongGuesses}`).classList.add('show-piece');
+		//losing path
+		//compare the letter guess to the word to see if it's in there
+		if (wrongGuesses >=5){
+
+		} else{
+			showResetScreen();
+
+		}
+
+wrongGuesses++;
+
 }
 
 // EVENT HANDLING
